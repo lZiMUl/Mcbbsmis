7b3273fd44abf0652231966cf996265bec0d247d
lZiMUl <lzimul@lzimul.com>
2026-03-14 21:59:34 -0700

    Release/1.2.3 (#110)
    ## [1.2.3] - 2026-03-14
    
    * upgrade(client): upgrade modules
    
    fix(client): modified the regular expression
    
    - Regular expression replace ```js (.)(\\S+)\\s(.*)')``` with ```js^(.)(\\S+)(?:\\s(.*))?$```
    
    ---------
    
    Signed-off-by: lZiMUl <lzimul@lzimul.com>

--- src/server/service/MinecraftService.ts
+++ src/server/service/MinecraftService.ts
@@ -93,10 +93,7 @@ class MinecraftService {
     }
   }
 
-  public async parseEventResult(
-    rawData: string,
-    bili: BiliSender
-  ): Promise<void> {
+  public parseEventResult(rawData: string, bili: BiliSender): void {
     const {
       body: { message, sender, type }
     }: IEventResult = JSON.parse(rawData);
@@ -118,7 +115,7 @@ class MinecraftService {
                   break;
                 case 'send':
                   {
-                    await bili?.send(
+                    bili?.send(
                       (this.resourcePack
                         ? content?.replace(
                             /[\uEF00-\uEF51]/g,
